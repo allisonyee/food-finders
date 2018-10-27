03029e021fedae78434dcc7cd0424e88bbd9dd26
Kevin Lin <kevin@kelda.io>
2018-10-27 10:52:15 -0700

    server: Implement endpoint to allow users to save bookmarks

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const PostgresClient = require('pg').Client;
 async function newApp() {
   const app = express();
   app.use(cors());
+  app.use(express.json());
 
   const yelpClient = yelp.client(secrets.yelpAPIKey);
 
@@ -70,10 +71,91 @@ async function newApp() {
     }
   });
 
-  app.post('/api/bookmarks/:id', async function(req, res) {
+  const requiredBookmarkFields = [];
+  const requiredBusinessFields = ['id'];
+  app.post('/api/bookmark', async function(req, res) {
+    const user = req.body.user;
+    if (!user) {
+      res.status(500);
+      res.json({ error: 'a user is required' });
+      return;
+    }
+
+    const bookmark = req.body.bookmark;
+    if (!bookmark) {
+      res.status(500);
+      res.json({ error: '`bookmark` is required' });
+      return;
+    }
+    if (!assertFields(res, 'bookmark', bookmark, requiredBookmarkFields)) {
+      return
+    }
+
+    const business = bookmark.business;
+    if (!business) {
+      res.status(500);
+      res.json({ error: '`business` is required' });
+      return;
+    }
+    if (!assertFields(res, 'business', business, requiredBusinessFields)) {
+      return
+    }
+
+    try {
+      await pgClient.query({
+        text: `INSERT INTO bookmarks (id, business_id, owner_id, tags)
+                VALUES ($1, $2, $3, $4)
+                ON CONFLICT (id)
+                DO
+                  UPDATE
+                    SET tags = EXCLUDED.tags`,
+        values: [
+          `${user}-${business.id}`,
+          business.id,
+          user,
+          bookmark.tags || [],
+        ],
+      });
+    } catch (err) {
+      res.status(500);
+      res.json({ error: `failed to save bookmark: ${err.toString()}` });
+      return;
+    }
+
+    try {
+      await pgClient.query({
+        text: `INSERT INTO businesses (id)
+                VALUES ($1)
+                ON CONFLICT (id) DO
+                  UPDATE
+                    SET id = EXCLUDED.id`,
+        values: [
+          business.id,
+        ],
+      });
+    } catch (err) {
+      res.status(500);
+      res.json({ error: `failed to save business: ${err.toString()}` });
+      return;
+    }
+
+    res.status(200);
+    res.json({ status: 'success' });
   });
 
   return app;
+}
+
+function assertFields(res, objectName, object, fields) {
+  for (let i = 0; i < fields.length; i++) {
+    const field = fields[i];
+    if (! (field in object)) {
+      res.status(500);
+      res.json({ error: `\`${objectName}.${field}\` is required` });
+      return false;
+    }
+  }
+  return true;
 }
 
 async function main() {

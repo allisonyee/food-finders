7318b6e5f5a0d7dc6535b4ad407c2e0be5fd352e
Allison Yee <ayeedawg@gmail.com>
2018-10-28 08:25:17 -0700

    parseAlias and getYelpBusiness

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,22 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('parseAlias', () => {
+  const app = new App();
+
+  it('should throw an error when a non-Yelp URL is entered', () => {
+    const url = 'https://www.google.com/';
+    expect(() => app.parseAlias(url)).toThrow();
+  });
+
+  it('should return the alias for a Yelp URL', () => {
+    const url = 'https://www.yelp.com/biz/benu-san-francisco-4';
+    expect(app.parseAlias(url)).toEqual('benu-san-francisco-4');
+  });
+
+  it('should return the alias for a Yelp URL with queries', () => {
+    const url = 'https://www.yelp.com/biz/cafe-europa-san-francisco?osq=food';
+    expect(app.parseAlias(url)).toEqual('cafe-europa-san-francisco');
+  });
+});

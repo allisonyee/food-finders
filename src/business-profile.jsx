27396d29a0e5f8f15202a47d12c6ec2697551c61
Allison Yee <ayeedawg@gmail.com>
2018-10-28 15:35:46 -0700

    Flesh out more proifle styles

--- src/business-profile.jsx
+++ src/business-profile.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { map } from 'lodash';
-import './styles/App.css';
+import './styles/business-profile.less';
 
 const propTypes = {
   business: PropTypes.shape({
@@ -20,21 +20,48 @@ class BusinessProfile extends Component {
     };
   }
 
+  renderAddress() {
+    const { location } = this.props.business;
+
+    return map(location.display_address, (line, index) => (
+      <div key={index} className="profile__address__line">
+        {line}
+      </div>
+    ));
+  }
+
+  renderTags() {
+    return (
+      <React.Fragment>
+        <h2 className="profile__section_header">Tags</h2>
+        <button className="profile__edit_btn">Edit</button>
+      </React.Fragment>
+    );
+  }
+
+  renderNotes() {
+    return (
+      <React.Fragment>
+        <h2 className="profile__section_header">Notes</h2>
+        <button className="profile__edit_btn">Edit</button>
+      </React.Fragment>
+    );
+  }
+
   render() {
-    const { name, location, photos } = this.props.business;
+    const { name, photos } = this.props.business;
 
     return (
       <div className="profile">
-        <h1>{name}</h1>
-        <div className="profile__photo">
-          <img src={photos[0]} />
-        </div>
-        <div className="profile__address">
-          {map(location.display_address, (line, index) => (
-            <div key={index} className="profile__address__line">
-              {line}
-            </div>
-          ))}
+        <div
+          className="profile__photo"
+          style={{ backgroundImage: `url(${photos[0]})` }}
+        />
+        <div className="profile__content">
+          <h1 className="profile__name">{name}</h1>
+          <div className="profile__address">{this.renderAddress()}</div>
+          <div className="profile__tags">{this.renderTags()}</div>
+          <div className="profile__notes">{this.renderNotes()}</div>
         </div>
       </div>
     );

78eaed2f4780e2565482177a3745e18a548bd1bb
Allison Yee <ayeedawg@gmail.com>
2018-10-28 21:16:01 -0700

    Move to AddBookmarkDialog

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { map, startsWith } from 'lodash';
-import BusinessProfile from './business-profile';
+import { map } from 'lodash';
 import './styles/App.css';
 
-class App extends Component {
+class FoodFinders extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      business: null,
-    };
 
     this.server = 'http://192.168.1.11:8888';
     this.urlInput = React.createRef();
@@ -39,63 +34,9 @@ class App extends Component {
     });
   }
 
-  getYelpBusiness(alias) {
-    return axios.get(`${this.server}/api/businesses/${alias}`).then(res => {
-      console.log(res);
-      return res.data;
-    });
-  }
-
-  onClick = event => {
-    event.preventDefault();
-    const url = this.urlInput.current.value;
-    this.getYelpBusiness(this.parseAlias(url)).then(business => {
-      this.setState({ business });
-    });
-  };
-
-  parseAlias(inputURL) {
-    const url = new URL(inputURL);
-    if (url.host.indexOf('yelp.com') === -1) throw new Error('Not a Yelp URL!');
-    if (startsWith(url.pathname, '/biz/')) return url.pathname.slice(5);
-    throw new Error('Invalid URL');
-  }
-
-  saveBookmark(user, business, { tags = null, notes = null }) {
-    axios
-      .post(`${this.server}/api/bookmark`, {
-        user,
-        business,
-        tags,
-        notes,
-      })
-      .then(resp => {
-        console.log(resp);
-        return resp;
-      });
-  }
-
   render() {
-    const { business } = this.state;
-
-    return (
-      <div className="App">
-        <form>
-          <label htmlFor="url">
-            Yelp URL
-            <input ref={this.urlInput} type="text" name="url" />
-          </label>
-          <button onClick={this.onClick} type="submit">
-            Save
-          </button>
-        </form>
-        <div>
-          <strong>Business data:</strong>
-          {business ? <BusinessProfile business={business} /> : 'Loading...'}
-        </div>
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
-export default App;
+export default FoodFinders;
---
+++ src/add-bookmark-dialog.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import { startsWith } from 'lodash';
+import './styles/App.css';
+
+const propTypes = {
+  server: PropTypes.string,
+};
+
+class AddBookmarkDialog extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      business: null,
+    };
+
+    this.urlInput = React.createRef();
+  }
+
+  getYelpBusiness(alias) {
+    return axios.get(`${this.server}/api/businesses/${alias}`).then(res => {
+      console.log(res);
+      return res.data;
+    });
+  }
+
+  onClick = event => {
+    event.preventDefault();
+    const url = this.urlInput.current.value;
+    this.getYelpBusiness(this.parseAlias(url)).then(business => {
+      this.setState({ business });
+    });
+  };
+
+  parseAlias(inputURL) {
+    const url = new URL(inputURL);
+    if (url.host.indexOf('yelp.com') === -1) throw new Error('Not a Yelp URL!');
+    if (startsWith(url.pathname, '/biz/')) return url.pathname.slice(5);
+    throw new Error('Invalid URL');
+  }
+
+  saveBookmark(user, business, { tags = null, notes = null }) {
+    axios
+      .post(`${this.server}/api/bookmark`, {
+        user,
+        business,
+        tags,
+        notes,
+      })
+      .then(resp => {
+        console.log(resp);
+        return resp;
+      });
+  }
+
+  render() {
+    const { business } = this.state;
+
+    return (
+      <form>
+        <label htmlFor="url">
+          Yelp URL
+          <input ref={this.urlInput} type="text" name="url" />
+        </label>
+        <button onClick={this.onClick} type="submit">
+          Save
+        </button>
+      </form>
+    );
+  }
+}
+
+AddBookmarkDialog.propTypes = propTypes;
+
+export default AddBookmarkDialog;
